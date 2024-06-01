6a964bcab779968022400aafaff23aeeed2f4cc5
U1 <U1@LAPTOP-701UTDT6>
2024-06-01 16:50:11 +0300

    oV0601_1650

--- DjangoTest500/settings.py
+++ DjangoTest500/settings.py
@@ -13,8 +13,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import posixpath
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
